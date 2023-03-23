c40a4dcd2f2be3f3d5c32353a9ec7eae754e7e18
FHAdrian <floreshadrian@gmail.com>
2023-03-23 12:27:42 -0300

    Respuestas en README.md, actualizacion de .gitignore y punto 3.e en tp1_1.c

--- tp1_1.c
+++ tp1_1.c
@@ -4,7 +4,18 @@
 
 int main(int argc, char const *argv[])
 {
-    printf("\n Hola Mundo \n");
-    
+    // printf("\n Hola Mundo \n");
+
+    int var = 20; //Declaro variable
+    int *ptr; //Declaro puntero
+
+    ptr = &var; //Asigno ubicacion de memora de var al puntero
+
+    printf("\n\n1.Contenido del puntero: %d \n", *ptr);
+    printf("2.La direccion de memmoria almacenada por el puntero: %d\n", ptr);
+    printf("3.La direccion de memoria de la variable: %d\n", &var);
+    printf("4.La direccion de memoria del puntero : %d\n", &ptr);
+    printf("5.El tamaño de memoria utilizado por la variable: %d\n", sizeof(var));
+
     return 0;
 }
